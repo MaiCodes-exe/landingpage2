c2fcdde62027fa66555a782e8f686a6bb46fc1ef
MaiCodes-exe <maise.b.costa@gmail.com>
2023-08-01 21:42:36 +0100

    added hero section

---
+++ my-app/src/herosection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
+
+const HeroSection = () => {
+  return (
+    <Box mt={4} textAlign="center">
+      <Typography variant="h2">Welcome to My Landing Page</Typography>
+      <Typography variant="subtitle1">
+        asdfghnjmk,jhgfdssxdfghjklkjhbgvfcdxsxdfghjkl,.,kjhg
+      </Typography>
+      <Button variant="contained" color="primary">
+        Get Started
+      </Button>
+    </Box>
+  );
+};
+
+export default HeroSection;
