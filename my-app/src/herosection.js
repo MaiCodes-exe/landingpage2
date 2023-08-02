bedee3944c64844d85678e3e5217644e8a010ac9
MaiCodes-exe <maise.b.costa@gmail.com>
2023-08-02 21:42:11 +0100

    adding text to sections

--- my-app/src/herosection.js
+++ my-app/src/herosection.js
@@ -4,7 +4,7 @@ import { Box, Typography, Button } from '@mui/material';
 const HeroSection = () => {
   return (
     <Box mt={4} textAlign="center">
-      <Typography variant="h2">Welcome to My Landing Page</Typography>
+      <Typography variant="h2">Landing Page</Typography>
       <Typography variant="subtitle1">
         asdfghnjmk,jhgfdssxdfghjklkjhbgvfcdxsxdfghjkl,.,kjhg
       </Typography>
