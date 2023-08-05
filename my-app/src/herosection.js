dab6a6b269bcfc78cd012ec75855faca24a40e86
MaiCodes-exe <maise.b.costa@gmail.com>
2023-08-05 21:21:10 +0100

    making adjustments

--- my-app/src/herosection.js
+++ my-app/src/herosection.js
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <Box mt={4} textAlign="center">
       <Typography variant="h2">Landing Page</Typography>
       <Typography variant="subtitle1">
-      editing      </Typography>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.    </Typography>
       <Button variant="contained" color="primary">
         Get Started
       </Button>
