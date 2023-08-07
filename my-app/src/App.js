26de1737aae9bd237b8117e3ffdfe59b12c5cf0a
MaiCodes-exe <maise.b.costa@gmail.com>
2023-08-07 21:25:28 +0100

    fixing importing errors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { AppBar, Container, Toolbar, Typography } from '@mui/material';
-import HeroSection from './components/HeroSection';
+import HeroSection from './herosection';
 // import FeaturesSection from './components/FeaturesSection';
 import Footer from './footer';
 
