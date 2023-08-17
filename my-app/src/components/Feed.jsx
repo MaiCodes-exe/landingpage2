aff9efe0363af975175e161baaa4dcd1fcca4234
MaiCodes-exe <81877052+MaiCodes-exe@users.noreply.github.com>
2023-08-17 19:20:01 +0100

    testing

--- my-app/src/components/Feed.jsx
+++ my-app/src/components/Feed.jsx
@@ -1,28 +1,7 @@
 import React from 'react'
-import Box from '@mui/material/Box';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-
-
-
-
-
-
-
-
-
-
-
 
 export const Feed = () => {
   return (
-    <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
-    <ImageList variant="masonry" cols={3} gap={8}>
-  
-        <ImageListItem>
-   
-        </ImageListItem>
-    
-    </ImageList>
-  </Box>  )
+    <div>Feed</div>
+  )
 }
