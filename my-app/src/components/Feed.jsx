5feec2e5a514a493a725f65950ee7a397aa601ed
MaiCodes-exe <81877052+MaiCodes-exe@users.noreply.github.com>
2023-08-17 15:24:56 +0100

    trying the image list component

--- my-app/src/components/Feed.jsx
+++ my-app/src/components/Feed.jsx
@@ -7,22 +7,22 @@ import ImageListItem from '@mui/material/ImageListItem';
 
 
 
+
+
+
+
+
+
+
 export const Feed = () => {
   return (
     <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
     <ImageList variant="masonry" cols={3} gap={8}>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
+  
+        <ImageListItem>
+   
         </ImageListItem>
-      ))}
+    
     </ImageList>
-  </Box>
-
-  )
+  </Box>  )
 }
